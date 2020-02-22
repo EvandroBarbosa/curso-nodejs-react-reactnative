4532734e329cd75a828fa9ed9d8126edeef27711
Evandro Barbosa <vandomarinho.vm@gmail.com>
2020-02-22 16:09:19 -0300

    Adicionando o React no componente raiz

---
+++ reactjs/src/App.js
@@ -0,0 +1,10 @@
+/* Toda arquivo em que for renderiza conteúdo JSX
+* é necessário importar o React
+*/
+import React from 'react'
+
+function App() {
+  return <h1>ErxSoluções!</h1>;
+}
+
+export default App;
--- reactjs/src/index.js
+++ reactjs/src/index.js
@@ -1,3 +1,9 @@
-const soma = (a, b) => a + b;
+// Importando o modulo do react
+import React from 'react'
+// Importando o react dom
+import { render } from 'react-dom'
 
-alert(soma(5, 9))
+// Importando o componente app
+import App from './App'
+
+render(<App />, document.getElementById('app'))
