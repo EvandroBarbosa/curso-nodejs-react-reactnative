151aa32d1368892395ed92a57ac174a486ac6004
Evandro Barbosa <vandomarinho.vm@gmail.com>
2020-02-24 11:47:33 -0300

    Conceito de defaultProp e PropTypes

--- reactjs/src/components/TechItem.js
+++ reactjs/src/components/TechItem.js
@@ -1,11 +1,25 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 function TechItem({ tech, onDelete }) {
   return(
-    <li>{tech}
+    <li>
+      {tech}
       <button type="button" onClick={onDelete} >Remover</button>
     </li>
   )
 }
+/* Conceito de default props
+ Essa propriedade é preenchida caso o usuário não informe nenhuma informação */
+/* TechItem.defaultProps = {
+  tech: "Oculta"
+} */
 
+/* Conceito de prop types 
+Quando é passado uma prop types como obrigatória, isso força o desenvolvedor
+a declarar essa propriedade no componente, assim o defaultprops se torna desnecessario*/
+TechItem.propTypes = {
+  tech: PropTypes.string,
+  onDelete: PropTypes.func.isRequired
+}
 export default TechItem;
