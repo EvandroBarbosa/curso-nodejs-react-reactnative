73e185a0bbcc88afae3db381508d10ba92909dd2
Evandro Barbosa <vandomarinho.vm@gmail.com>
2020-03-06 13:31:21 -0300

    Adicionando a funçao de loading

--- reactnative/src/pages/Main/index.js
+++ reactnative/src/pages/Main/index.js
@@ -1,23 +1,97 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import { Keyboard, ActivityIndicator } from 'react-native';
+import Api from '../../server/Api';
 
-import { Container, Form, Input, SubmitButton } from './styles';
+import {
+  Container,
+  Form,
+  Input,
+  SubmitButton,
+  List,
+  User,
+  Name,
+  Avatar,
+  Bio,
+  ProfileButton,
+  ProfileButtonAvatar,
+} from './styles';
 
-export default function Main() {
-  return (
-    <Container>
-      <Form>
-        <Input
-          autoCorrect={false}
-          autoCapitalize="none"
-          placeholder="Adicionar usuário"
+export default class Main extends Component {
+  state = {
+    newUser: '',
+    users: [],
+    loading: false,
+  };
+
+  // Method de adicionar o usuário
+  handleAddUser = async () => {
+    const { users, newUser } = this.state;
+
+    this.setState({ loading: true });
+
+    // Pega a resposta da requisição na API do github
+    const response = await Api.get(`/users/${newUser}`);
+
+    // Preenche o objeto data com os dados vindo da API
+    const data = {
+      name: response.data.name,
+      login: response.data.login,
+      bio: response.data.bio,
+      avatar: response.data.avatar_url,
+    };
+
+    // Seta esses dados no nosso state
+    this.setState({
+      users: [...users, data],
+      newUser: '',
+      loading: false,
+    });
+
+    Keyboard.dismiss();
+  };
+
+  render() {
+    const { users, newUser, loading } = this.state;
+    return (
+      <Container>
+        <Form>
+          <Input
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Adicionar usuário"
+            value={newUser}
+            onChangeText={text => this.setState({ newUser: text })}
+            returnKeyType="send"
+            onSubmitEditing={this.handleAddUser}
+          />
+          <SubmitButton loading={loading} onPress={this.handleAddUser}>
+            {loading ? (
+              <ActivityIndicator color="#fff" />
+            ) : (
+              <Icon name="add" size={20} color="#fff" />
+            )}
+          </SubmitButton>
+        </Form>
+
+        <List
+          data={users}
+          keyExtractor={user => user.login}
+          renderItem={({ item }) => (
+            <User>
+              <Avatar source={{ uri: item.avatar }} />
+              <Name>{item.name}</Name>
+              <Bio>{item.bio}</Bio>
+
+              <ProfileButton onPress={() => {}}>
+                <ProfileButtonAvatar>Ver perfil</ProfileButtonAvatar>
+              </ProfileButton>
+            </User>
+          )}
         />
-        <SubmitButton>
-          <Icon name="add" size={20} color="#fff" />
-        </SubmitButton>
-      </Form>
-    </Container>
-  );
+      </Container>
+    );
+  }
 }
 
 Main.navigationOptions = {
---
+++ reactnative/src/server/Api.js
@@ -0,0 +1,6 @@
+import axios from 'axios';
+
+const Api = axios.create({
+  baseURL: 'https://api.github.com',
+});
+export default Api;
