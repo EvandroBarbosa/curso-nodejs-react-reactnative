a6b464eeaea3c4aab58ccd777f1d855fc3df90f8
Evandro Barbosa <vandomarinho.vm@gmail.com>
2020-03-06 16:01:32 -0300

    Finalizando a aplicação de lista usuarios do github e mostrar seus repositorios favoritos com ReactNative

--- reactnative/src/pages/User/index.js
+++ reactnative/src/pages/User/index.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
+import { ActivityIndicator } from 'react-native';
 import PropTypes from 'prop-types';
 import Api from '../../server/Api';
 
-import { Container } from './styles';
+import {
+  Container,
+  Header,
+  Avatar,
+  Name,
+  Bio,
+  Stars,
+  Starred,
+  OwnerAvatar,
+  Info,
+  Title,
+  Author,
+} from './styles';
 
 export default class User extends Component {
   static navigationOptions = ({ navigation }) => ({
@@ -17,19 +30,51 @@ export default class User extends Component {
 
   state = {
     stars: [],
+    loading: false,
   };
 
   async componentDidMount() {
     const { navigation } = this.props;
     const user = navigation.getParam('user');
+    this.setState({ loading: true });
 
     const response = await Api.get(`/users/${user.login}/starred`);
 
-    this.setState({ stars: response.data });
+    this.setState({ stars: response.data, loading: false });
   }
 
   render() {
-    const { stars } = this.state;
-    return <Container />;
+    const { navigation } = this.props;
+    const { stars, loading } = this.state;
+
+    const user = navigation.getParam('user');
+
+    return (
+      <Container loading={loading}>
+        <Header>
+          <Avatar source={{ uri: user.avatar }} />
+          <Name>{user.name}</Name>
+          <Bio>{user.bio}</Bio>
+        </Header>
+
+        {loading ? (
+          <ActivityIndicator color="#1360ed" size={100} />
+        ) : (
+          <Stars
+            data={stars}
+            keyExtractor={star => String(star.id)}
+            renderItem={({ item }) => (
+              <Starred>
+                <OwnerAvatar source={{ uri: item.owner.avatar_url }} />
+                <Info>
+                  <Title>{item.name}</Title>
+                  <Author>{item.owner.login}</Author>
+                </Info>
+              </Starred>
+            )}
+          />
+        )}
+      </Container>
+    );
   }
 }
--- reactnative/src/pages/User/styles.js
+++ reactnative/src/pages/User/styles.js
@@ -1,3 +1,68 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  flex: 1;
+  padding: 30px;
+  opacity: ${props => (props.loading ? 0.7 : 1)};
+`;
+
+export const Header = styled.View`
+  align-items: center;
+  padding-bottom: 20px;
+  border-right-width: 1px;
+  border-color: #eee;
+`;
+export const Avatar = styled.Image`
+  width: 100px;
+  height: 100px;
+  border-radius: 50px;
+  background: #eee;
+`;
+export const Name = styled.Text`
+  font-size: 20px;
+  font-weight: bold;
+  color: #333;
+  margin-top: 10px;
+  text-align: center;
+`;
+export const Bio = styled.Text`
+  font-size: 14px;
+  line-height: 18px;
+  color: #999;
+  text-align: center;
+`;
+
+export const Stars = styled.FlatList.attrs({
+  showsVerticalScrollIndicator: false,
+})`
+  margin-top: 20px;
+`;
+
+export const Starred = styled.View`
+  background-color: #f5f5f5;
+  border-radius: 4px;
+  flex-direction: row;
+  margin-bottom: 20px;
+  padding: 10px 15px;
+  align-items: center;
+`;
+export const OwnerAvatar = styled.Image`
+  width: 42px;
+  height: 42px;
+  border-radius: 21px;
+  background: #eee;
+`;
+export const Info = styled.View`
+  margin-left: 10px;
+  flex: 1;
+`;
+export const Title = styled.Text`
+  font-size: 15px;
+  font-weight: bold;
+  color: #333;
+`;
+export const Author = styled.Text`
+  font-size: 13px;
+  color: #666;
+  margin-top: 2px;
+`;
