2c4d7a7aa4a5c6e6b082b448846de54aa0507780
Evandro Barbosa <vandomarinho.vm@gmail.com>
2020-03-17 15:27:38 -0300

    Conhecendo os Hooks useState e useEffect e aplicando

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,7 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  return <h1>Hello world!</h1>;
+  // Usagem do Hook useState!!!
+  const [tech, setTech] = useState([]);
+  const [newTech, setNewTech] = useState('');
+
+  // Usage do Hook useEffect
+  // Simula o componentDidMount()
+  useEffect(() => {
+    const storageTech = localStorage.getItem('tech');
+
+    if (storageTech) {
+      setTech(JSON.parse(storageTech));
+    }
+  }, []);
+
+  // Simula o componentDidUpdate()
+  useEffect(() => {
+    localStorage.setItem('tech', JSON.stringify(tech));
+  }, [tech]);
+
+  function handleAdd() {
+    setTech([...tech, newTech]);
+    setNewTech('');
+  }
+
+  return (
+    <>
+      <ul>
+        {tech.map(t => (
+          <li key={t}>{t}</li>
+        ))}
+      </ul>
+      <input
+        type="text"
+        value={newTech}
+        onChange={e => setNewTech(e.target.value)}
+      />
+      <button type="button" onClick={handleAdd}>
+        +
+      </button>
+    </>
+  );
 }
 
 export default App;
