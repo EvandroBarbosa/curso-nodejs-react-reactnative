b91d4175db6f5d92985e4a5025b57f894e68ac2b
Evandro Barbosa <vandomarinho.vm@gmail.com>
2020-03-19 10:56:02 -0300

    Configurando a estrutura do projeto, rotas, layout

---
+++ front-modulo02/.eslintrc.js
@@ -0,0 +1,58 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    jest: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'prettier',
+    'prettier/react'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+    __DEV__: true
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    'prettier',
+    'import',
+    'jsx-a11y',
+    'react-hooks'
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': [
+      'warn',
+      { extensions: ['.jsx', '.js']}
+    ],
+    'import/prefer-default-export': 'off',
+    'no-unused-vars': ['error', {argsIgnorePattern: '^_'}],
+    'no-param-reassign': 'off',
+    'no-console': ['error', { allow: ['tron']}],
+    'react/jsx-one-expression-per-line': 'off',
+    'global-require': 'off',
+    'no-underscore-dangle': 'off',
+    camelcase: 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/jsx-props-no-spreading': 'off'
+  },
+  settings: {
+    'import/resolver': {
+      'babel-plugin-root-import': {
+        rootPathSuffix: 'src'
+      }
+    }
+  }
+};
---
+++ front-modulo02/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import { Router } from 'react-router-dom';
+
+import GlobalStyles from './styles/global';
+
+import './config/Reactotron';
+
+import Routes from './routes';
+import history from './services/history';
+
+function App() {
+  return (
+    <Router history={history}>
+      <Routes />
+      <GlobalStyles />
+    </Router>
+  );
+}
+
+export default App;
