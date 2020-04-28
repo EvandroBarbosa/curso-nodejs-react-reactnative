3fea089bd979a353f54f3d0e6069060377f402ec
Evandro Barbosa <vandomarinho.vm@gmail.com>
2020-04-28 10:53:58 -0300

    Concluindo o App do projeto gobarber

--- mobile/src/pages/Dashboard/index.js
+++ mobile/src/pages/Dashboard/index.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import { withNavigationFocus } from 'react-navigation';
 import api from '~/services/api';
 
 import Background from '~/components/Background';
@@ -7,29 +8,30 @@ import Appointment from '~/components/Appointment';
 
 import { Container, Title, List } from './styles';
 
-const data = [1, 2, 3, 4, 5];
-
-export default function Dashboard() {
+function Dashboard({ isFocused }) {
   const [appointments, setAppointments] = useState([]);
 
+  async function loadAppointment() {
+    const response = await api.get('appointments');
+
+    setAppointments(response.data);
+  }
+
   useEffect(() => {
-    async function loadAppointment() {
-      const response = await api.get('appointments');
-
-      setAppointments(response.data);
-    }
-
-    loadAppointment();
-  }, []);
+    if (isFocused) loadAppointment();
+  }, [isFocused]);
 
   async function handleCancel(id) {
     const response = await api.delete(`appointments/${id}`);
 
     setAppointments(
-      appointments.map((appontment) =>
-        appontment.id === id
-          ? { ...appontment, canceled_at: response.data.canceled_at }
-          : appontment
+      appointments.map((appointment) =>
+        appointment.id === id
+          ? {
+              ...appointment,
+              canceled_at: response.data.canceled_at,
+            }
+          : appointment
       )
     );
   }
@@ -57,3 +59,5 @@ Dashboard.navigationOptions = {
     <Icon name="event" size={20} color={tintColor} />
   ),
 };
+
+export default withNavigationFocus(Dashboard);
